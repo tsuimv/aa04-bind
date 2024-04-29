6ee5cf9dcdc155ee2d0f27f3fff878131b6b1b6f
Mitchell Tsui <tsuimv@DESKTOP-CR6MCST.localdomain>
2024-04-29 19:42:11 -0400

    final

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-// Your code here 
+const Employee = require("/home/tsuimv/AppAcademyWork/aa04-bind/employee")
+
+let employee = new Employee('John Wick', 'Dog Lover')
+let sayNameFunc = employee.sayName
+let sayName = sayNameFunc.bind(employee)
+// sayName()
+let bound = employee.sayName.bind(employee)
+global.setTimeout(bound, 2000)
+
+sayOccupationFunc = employee.sayOccupation
+let oBound = employee.sayOccupation.bind(employee)
+global.setTimeout(oBound, 3000) 
